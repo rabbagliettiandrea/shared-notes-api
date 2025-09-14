2a5bad11cc6bf0dbef832ebaab391b2e53d2ca4d
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2025-09-14 13:21:44 +0200

    fix: correct application configuration settings

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,15 @@ import os
 
 
 class Settings(BaseSettings):
+    DEBUG: bool
+    DATABASE_URL: str
+    REDIS_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_DAYS: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    ALLOWED_HOSTS: str
+    
     class Config:
         env_file = [".env"]
         case_sensitive = True
